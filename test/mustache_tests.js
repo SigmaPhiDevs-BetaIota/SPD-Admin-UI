24f6879da0c805a6932ab054a0f5f13f22aa1890
Cole Alban <colemalban@gmail.com>
2016-08-14 19:43:36 -0400

    Create mustache tests"

---
+++ test/mustache_tests.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+
+describe('Test load file function', function(){
+    
+    it('Should load index.html', function(){
+        expect(true).to.be.true;
+    });
+});
